c1a96ad8f50b0dde1280e764db80fd765a2d276d
OlgaMrozek <mrozek.olga@gmail.com>
2025-06-06 16:29:34 +0200

    Dodano projekt wypożyczalni rowerów

---
+++ wypozyczalnia-rowerow/src/main/java/projekt_java/App.java
@@ -0,0 +1,13 @@
+package projekt_java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
